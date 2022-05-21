16396635da21214010d5f2a59558be2fbd0c521f
Erik-TW <45933770+Erik-TW@users.noreply.github.com>
2022-05-21 14:39:58 +0200

    Setup of shell almost done

---
+++ CodeLibrary.Data/Services/BookService.cs
@@ -0,0 +1,30 @@
+﻿using CodeLibrary.Data.Context;
+using CodeLibrary.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLibrary.Data.Services
+{
+
+    public interface IBookService
+    {
+        public Task<IEnumerable<Book>> GetBooks();
+    }
+    public class BookService : IBookService
+    {
+        private CodeLibraryDbContext _context;
+        public BookService(CodeLibraryDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _context.Books.ToListAsync();
+        }
+    }
+}
---
+++ CodeLibrary/Controllers/BooksController.cs
@@ -0,0 +1,25 @@
+﻿using CodeLibrary.Data.Models;
+using CodeLibrary.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeLibrary.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookService.GetBooks();
+        }
+    }
+}
